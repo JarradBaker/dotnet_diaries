432d4cf7988f9a301ba01cd2f0560ab54a845e2d
Jarrad <jarradbaker@outlook.com>
2025-05-13 21:30:59 +0100

    Initial Commit

---
+++ dotnet_diaries/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_diaries.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
